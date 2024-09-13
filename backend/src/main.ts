497f2d7442b50472f3a9d625b88f8c813b844711
Benjamin Grolleau <benjamin.grolleau@outlook.com>
2024-09-13 11:10:43 +0200

    Production mode

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,20 @@ const port = 3006;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const allowedOrigins = ['http://localhost:3005', 'bae.bgrolleau.com'];
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }, 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  })
+  
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
   console.log(`App is listening on port ${port}`)
